f00ec5f55e224350443d4c3f49971b5e286f60ed
Corey Staples <CESCoding@gmail.com>
2019-09-21 20:28:03 -0500

    Initial Commit: Midway through part 2 of tutorial. Next relevant paragraph start: Now that we have the tile class, we need some sort of container to hold our tiles. http://rogueliketutorials.com/tutorials/tcod/part-2/

--- engine.py
+++ engine.py
@@ -1,11 +1,16 @@
 import tcod as libtcod
 from input_handlers import handle_keys
+from entity import Entity
+from render_functions import render_all, clear_all
 
 def main():
     screen_width = 80
     screen_height = 50
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height/ 2)
+    map_width = 80
+    map_height = 45
+    player = Entity(int(screen_width/2), int(screen_height/2), '@', libtcod.white)
+    npc = Entity(int(screen_width/2 - 5), int(screen_height/2), '@', libtcod.yellow)
+    entities = [npc, player]
     key = libtcod.Key()
     mouse = libtcod.Mouse()
 
@@ -18,12 +23,11 @@ def main():
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
-        libtcod.console_set_default_foreground(con, libtcod.white)
-        libtcod.console_put_char(con, player_x, player_y, '@', libtcod.BKGND_NONE)
-        libtcod.console_blit(con,0,0,screen_width,screen_height,0,0,0)
+        render_all(con, entities, screen_width, screen_height)
+
         libtcod.console_flush()
 
-        libtcod.console_put_char(con, player_x, player_y, ' ', libtcod.BKGND_NONE)
+        clear_all(con, entities)
 
         action = handle_keys(key)
 
@@ -31,6 +35,9 @@ def main():
         exit = action.get('exit')
         fullscreen = action.get('fullscreen')
 
+        if move:
+            dx, dy = move
+            player.move(dx,dy)
 
         if exit:
             return True
